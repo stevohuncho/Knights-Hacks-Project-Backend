5b4fb4c946ecc2794de9d32c80bbd7d17e9f81be
stevo <93010314+stevohuncho@users.noreply.github.com>
2023-02-20 23:52:31 -0500

    added restuarant data api

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 from flask import Flask, request
 
 # import functions from scrape.py
-from scrape import scrapeRestaurantData
+from scrape import getRestaurantsData, getRestaurantData
 
 server = Flask(__name__)
 
@@ -11,9 +11,10 @@ server = Flask(__name__)
 def hello():
     return "hello", 200
 
-# webscrape all restaurant data near a users location
-@server.route("/scrape", methods=["GET"])
-def scrape():
+# gets restaurants data near user
+# example http://localhost:5001/nearme?lat=28.5970378&long=-81.2276083
+@server.route("/nearme", methods=["GET"])
+def nearme():
     # get latitude and longitude from query params
     latParams = request.args.getlist('lat')
     longParams = request.args.getlist('long')
@@ -22,9 +23,36 @@ def scrape():
     if len(latParams) != 1 or len(longParams) != 1:
         return "insufficient or excessive query params", 400
 
-    scrapeRestaurantData(latParams[0], longParams[0])
+    # get data
+    restaurantsData = getRestaurantsData(latParams[0], longParams[0])
 
-    return "success", 200
+    # check if response is valid
+    if restaurantsData:
+        return restaurantsData, 200
+
+    # invalid response
+    return "error while finding restaurant data"
+
+# get restaurant data in depth
+# example http://localhost:5001/restaurant?id=ChIJNX-91oxo54gRDzDmAJVGFRM
+@server.route("/restaurant", methods=["GET"])
+def restaurant():
+    # get id from query params
+    idParams = request.args.getlist('id')
+
+    if len(idParams) != 1:
+        return "insufficient or excessive query params", 400
+
+    # get data
+    restaurantData = getRestaurantData(idParams[0])
+
+    # check if response is valid
+    if restaurantData:
+        return restaurantData, 200
+
+    # invalid response
+    return "error while finding restaurant data"
+
 
 
 if __name__ == "__main__":
